86da1f448755ff58509ce3e6dffb1c3c7cac2cf0
Pedro-ss <pedross.tool@gmail.com>
2021-03-18 18:18:39 -0300

    iniciando projeto sobre RecyclerView

---
+++ app/src/main/java/com/example/bootcampapp/Contact.kt
@@ -0,0 +1,7 @@
+package com.example.bootcampapp
+
+data class Contact (
+    var nome: String,
+    var phone: String,
+    var photograph: String
+    )
---
+++ app/src/main/java/com/example/bootcampapp/ContactAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.bootcampapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
+
+    private val list: MutableList<Contact> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
+        //val view = LayoutInflater.from(parent.context).inflate()
+
+    }
+
+    override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        fun bind(contact: Contact){
+
+        }
+    }
+}
